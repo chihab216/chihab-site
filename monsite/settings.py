bca32ec68b6aa3e633d72d0354d89c2273d50fd0
chihab216 <chihab.khnifass@mines-ales.org>
2020-09-24 17:01:05 -0700

    first commit

--- monsite/settings.py
+++ monsite/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
-import dj-database-url
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,6 +141,6 @@ if os.environ.get('ENV') == 'PRODUCTION':
 
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-    db_from_env = dj-database-url.config(conn_max_age=500)
+    db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
    
